12ce140c013b02041b46b5f3bde7456ba7ab1ed0
Bryan <lumanlan.jbg@gmail.com>
2020-11-28 21:19:35 +0800

    custom functions and send, add views

--- project/app/urls.py
+++ project/app/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
 	path('login', views.login, name='login'),
-    path('home', views.home, name='home'),
     path('send', views.send, name='send'),
     path('analyze', views.analyze, name='analyze'),
-    path('view', views.view, name='view')
+    path('view', views.view, name='view'),
+    path('add', views.add, name='add'),
+    path('dashboard', views.dashboard, name='dashboard'),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -4,16 +4,21 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def login(request):
-	return render(request, 'app/login.html', {})
+	return render(request, 'app/login.html', {'title':' - Login','active':'Login'})
 
-def home(request):
-	return render(request, 'app/home.html', {})
+def dashboard(request):
+	return render(request, 'app/dashboard.html', {'title':' - Dashboard','active':'Dashboard'})
 
 def send(request):
-	return render(request, 'app/send.html', {'title':'Send Results'})
+	return render(request, 'app/send.html', {'title':' - Send Results','active':'Send'})
 
 def analyze(request):
-	return render(request, 'app/analyze.html', {})
+	return render(request, 'app/analyze.html', {'title':' - Analyze CT Scan','active':'Analyze'})
 
 def view(request):
-	return render(request, 'app/view.html', {})
+	return render(request, 'app/view.html', {'title':' - View Records','active':'View'})
+
+def add(request):
+	return render(request, 'app/add.html', {'title':' - Add Patient','active':'Add'})
+
+
