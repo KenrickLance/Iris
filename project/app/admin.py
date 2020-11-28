ac5b31893e99088b1ef990f628c2f79580186cf2
Lance B <kenricklance092@gmail.com>
2020-11-28 19:21:23 +0800

    stuff

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from .models import Hospital, Patient, Doctor, Record
 
 admin.site.register(Hospital)
--- project/app/models.py
+++ project/app/models.py
@@ -1,26 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 def make_file_path_name(instance, filename):
 	return f'patient{instance.patient.id}/{filename}'
 
 class Hospital(models.Model):
-    name = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=32)
+	name = models.CharField(max_length=100)
+	username = models.CharField(max_length=100)
+	password = models.CharField(max_length=32)
 
 class Patient(models.Model):
-    lastname = models.CharField(max_length=100)
-    firstname = models.CharField(max_length=100)
-    middlename = models.CharField(max_length=100)
-    phone = models.CharField(max_length=20)
-    email = models.EmailField()
+	lastname = models.CharField(max_length=100)
+	firstname = models.CharField(max_length=100)
+	middlename = models.CharField(max_length=100)
+	phone = models.CharField(max_length=20)
+	email = models.EmailField()
 
 class Doctor(models.Model):
 	hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
 	lastname = models.CharField(max_length=100)
 	firstname = models.CharField(max_length=100)
 	middlename = models.CharField(max_length=100)
-	
+
 class Record(models.Model):
 	patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
 	doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
