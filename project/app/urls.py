47a02be287715073dd9e4f64c87666cb8b58a5e0
Lance B <kenricklance092@gmail.com>
2020-11-28 16:08:10 +0800

    Created sqlite database. Fixed bug in app/urls.py

--- project/app/urls.py
+++ project/app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
 	path('/login', views.login, name='login'),
     path('/home', views.home, name='home'),
     path('/send', views.scan, name='scan'),
-    path('/analyze', views.analyze, name='analyze')
+    path('/analyze', views.analyze, name='analyze'),
     path('/view', views.view, name='view')
 ]
