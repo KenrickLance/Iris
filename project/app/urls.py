848ac0a89d23e59e07ff26798ef60ff7799b0d2e
Lance B <kenricklance092@gmail.com>
2020-11-29 05:33:47 +0800

    bugfix

--- project/app/urls.py
+++ project/app/urls.py
@@ -11,17 +11,17 @@ router.register(r'user', views.api_user, basename='user')
 
 urlpatterns = [
 	path('login', views.login, name='login'),
-    path('send', views.send, name='send'),
-    path('analyze', views.analyze, name='analyze'),
-    path('view', views.view, name='view'),
-    path('add', views.add, name='add'),
-    path('dashboard', views.dashboard, name='dashboard'),
-    path('logout', views.logout, name='logout'),
-    path('download', views.download, name='download'),
-    path('get_file', views.get_file, name='get_file')
+	path('send', views.send, name='send'),
+	path('analyze', views.analyze, name='analyze'),
+	path('view', views.view, name='view'),
+	path('add', views.add, name='add'),
+	path('dashboard', views.dashboard, name='dashboard'),
+	path('logout', views.logout, name='logout'),
+	path('download', views.download, name='download'),
+	path('get_file', views.get_file, name='get_file'),
 
-    path('api/', include(router.urls)),
-    path('api/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+	path('api/', include(router.urls)),
+	path('api/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
-    path('api/login', views.api_login.as_view(), name='api_login'),
+	path('api/login', views.api_login.as_view(), name='api_login'),
 ]
